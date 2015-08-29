1d7ee559281b6d0eb2353902bfed2b1fc8873dd4
Guo Xiang Tan <tgx_world@hotmail.com>
2015-08-29 14:09:57 +0800

    FIX: Failing specs.

--- spec/controllers/translator_controller_spec.rb
+++ spec/controllers/translator_controller_spec.rb
@@ -15,31 +15,31 @@ describe ::DiscourseTranslator::TranslatorController do
       before { SiteSetting.translator_enabled = false }
 
       it 'should deny request to translate' do
-        expect{ xhr :put, :translate, { post_id: 1 } }
+        expect{ xhr :post, :translate, { post_id: 1 } }
           .to raise_error ApplicationController::PluginDisabled
       end
     end
 
     describe "when enabled" do
-      let(:post) { Fabricate(:post) }
+      let(:reply) { Fabricate(:post) }
 
       it 'raises an error with a missing parameter' do
-        expect{ xhr :put, :translate }
+        expect{ xhr :post, :translate }
           .to raise_error(ActionController::ParameterMissing)
       end
 
-      it 'rescues all exceptions' do
-        DiscourseTranslator::Microsoft.expects(:translate).raises(StandardError)
+      it 'rescues translator errors' do
+        DiscourseTranslator::Microsoft.expects(:translate).raises(::DiscourseTranslator::TranslatorError)
 
-        xhr :put, :translate, { post_id: post.id }
+        xhr :post, :translate, { post_id: reply.id }
 
         expect(response).to have_http_status(422)
       end
 
       it 'returns the translated text' do
-        DiscourseTranslator::Microsoft.expects(:translate).with(post).returns(['ja', 'ニャン猫'])
+        DiscourseTranslator::Microsoft.expects(:translate).with(reply).returns(['ja', 'ニャン猫'])
 
-        xhr :put, :translate, { post_id: post.id }
+        xhr :post, :translate, { post_id: reply.id }
 
         expect(response).to be_success
         expect(response.body).to eq({ translation: 'ニャン猫', detected_lang: 'ja' }.to_json)
