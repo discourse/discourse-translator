e407f99cb298d542a63801bf6e66013c20264bb9
Juan David Martinez <juan@discourse.org>
2023-05-17 14:23:19 -0500

    FEATURE: Restrict translations by poster group

--- lib/discourse_translator/guardian_extension.rb
+++ lib/discourse_translator/guardian_extension.rb
@@ -2,9 +2,22 @@
 module DiscourseTranslator::GuardianExtension
   def user_group_allowed?
     authorized_groups = SiteSetting.restrict_translation_by_group.split("|").map(&:to_i)
+    
+    authorized? current_user, authorized_groups
+  end
 
-    return false if !current_user
-    user_groups = current_user.groups.pluck :id
+  def post_group_allowed? (post)
+    authorized_poster_groups = SiteSetting.restrict_translation_by_poster_group.split("|").map(&:to_i)
+    return true if authorized_poster_groups.empty?
+    
+    poster = User.find post.user_id
+    authorized? poster, authorized_poster_groups
+  end
+
+  def authorized? (user, authorized_groups)
+    return false if !user
+    
+    user_groups = user.groups.pluck :id
     authorized = authorized_groups.intersection user_groups
     !authorized.empty?
   end
