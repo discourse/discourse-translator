98290368466c44939f21903cab872fb1a7e77270
Jarek Radosz <jradosz@gmail.com>
2023-11-29 19:39:21 +0100

    DEV: Update linting

--- assets/javascripts/discourse/components/translated-post.js
+++ assets/javascripts/discourse/components/translated-post.js
@@ -1,5 +1,5 @@
-import computed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
+import computed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   @computed("post.translated_text")
