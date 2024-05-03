7e89003dfdddf602257bd5d64bb3367334e052a6
Nat <natalie.tay@discourse.org>
2024-05-03 13:12:15 +0800

    Remove unnecessary bangsss

--- lib/discourse_translator/guardian_extension.rb
+++ lib/discourse_translator/guardian_extension.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 module DiscourseTranslator::GuardianExtension
   def user_group_allow_translate?
-    !!current_user&.in_any_groups?(SiteSetting.restrict_translation_by_group_map)
+    return false if !current_user
+    current_user.in_any_groups?(SiteSetting.restrict_translation_by_group_map)
   end
 
   def poster_group_allow_translate?(post)
     return false if !current_user
     return true if SiteSetting.restrict_translation_by_poster_group_map.empty?
-    !!post.user.in_any_groups?(SiteSetting.restrict_translation_by_poster_group_map)
+    post.user.in_any_groups?(SiteSetting.restrict_translation_by_poster_group_map)
   end
 end
