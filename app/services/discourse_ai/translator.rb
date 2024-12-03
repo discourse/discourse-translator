5aa9492acc533a7dad68f5110880fa78782af889
Nat <natalie.tay@discourse.org>
2024-12-03 17:58:00 +0800

    Improve prompt so that it preserves html tags

--- app/services/discourse_ai/translator.rb
+++ app/services/discourse_ai/translator.rb
@@ -3,10 +3,10 @@
 module DiscourseAi
   class Translator
     PROMPT_TEMPLATE = <<~TEXT.freeze
-      You are a highly skilled translator with expertise in many languages.
+      You are a highly skilled linguist and web programmer, with expertise in many languages, and very well versed in HTML.
       Your task is to identify the language of the text I provide and accurately translate it into this language locale "%{target_language}" while preserving the meaning, tone, and nuance of the original text.
-      The text may also contain html tags, which should be preserved in the translation.
-      Please maintain proper grammar, spelling, and punctuation in the translated version.
+      The text will contain html tags, which must absolutely be preserved in the translation.
+      Maintain proper grammar, spelling, and punctuation in the translated version.
       Wrap the translated text in a <translation> tag.
     TEXT
 
