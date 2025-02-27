f6ea0b4a26835b6a6d07d32db041a11abe85e90f
Natalie Tay <natalie.tay@gmail.com>
2025-02-27 20:56:30 +0800

    UX: Filter only content-translated languages for the language switcher (#235)
    The `experimental_anon_language_switcher` will now rely on values in `automatic_translation_target_languages`. The language switcher cannot be enabled, and will not show up if there are no languages set.

--- app/validators/language_switcher_setting_validator.rb
+++ app/validators/language_switcher_setting_validator.rb
@@ -6,11 +6,12 @@ class LanguageSwitcherSettingValidator
   end
 
   def valid_value?(val)
-    return true if val == "f"
-    SiteSetting.set_locale_from_cookie
+    return true if val == "f" || val == "false"
+    SiteSetting.set_locale_from_cookie &&
+      SiteSetting.automatic_translation_target_languages.present?
   end
 
   def error_message
-    I18n.t("site_settings.errors.set_locale_cookie_requirements")
+    I18n.t("site_settings.errors.experimental_anon_language_switcher_requirements")
   end
 end
