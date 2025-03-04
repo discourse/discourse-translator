5f5425eea6aadbcd0afa27d42e63af16bd9cd5dc
Nat <natalie.tay@discourse.org>
2025-03-04 12:23:08 +0800

    FIX: Incorrect lang mapping using non-symbols
    Also adds test coverage for /translate endpoint

--- spec/services/libre_translate_spec.rb
+++ spec/services/libre_translate_spec.rb
@@ -4,6 +4,9 @@ require "rails_helper"
 
 RSpec.describe DiscourseTranslator::LibreTranslate do
   let(:mock_response) { Struct.new(:status, :body) }
+  let(:api_key) { "12345" }
+
+  before { SiteSetting.translator_libretranslate_endpoint = "http://localhost:5000" }
 
   describe ".access_token" do
     describe "when set" do
@@ -17,7 +20,6 @@ RSpec.describe DiscourseTranslator::LibreTranslate do
 
   describe ".translate_supported?" do
     it "should equate source language to target" do
-      SiteSetting.translator_libretranslate_endpoint = "http://localhost:5000"
       source = "en"
       target = :fr
 
@@ -25,6 +27,40 @@ RSpec.describe DiscourseTranslator::LibreTranslate do
 
       Excon.expects(:get).returns(mock_response.new(200, data.to_json))
       expect(described_class.translate_supported?(source, target)).to be true
+    end
+  end
+
+  describe ".translate" do
+    fab!(:post)
+
+    before do
+      SiteSetting.translator_libretranslate_api_key = api_key
+      Excon
+        .expects(:get)
+        .with(SiteSetting.translator_libretranslate_endpoint + "/languages")
+        .returns(mock_response.new(200, [{ code: "de" }, { code: "en" }].to_json))
+    end
+
+    it "truncates text for translation to max_characters_per_translation setting" do
+      SiteSetting.max_characters_per_translation = 50
+      post.set_detected_locale("de")
+      body = { q: post.cooked, source: "de", target: "en", format: "html", api_key: api_key }
+
+      translated_text = "hur dur hur dur"
+      # https://publicapi.dev/libre-translate-api
+      Excon
+        .expects(:post)
+        .with(
+          SiteSetting.translator_libretranslate_endpoint + "/translate",
+          body: URI.encode_www_form(body),
+          headers: {
+            "Content-Type" => "application/x-www-form-urlencoded",
+          },
+        )
+        .returns(mock_response.new(200, %{ { "translatedText": "#{translated_text}"} }))
+        .once
+
+      expect(described_class.translate(post)).to eq(["de", translated_text])
     end
   end
 end
