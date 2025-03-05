7a7310dd564276450e58dd9a6a695483c97192e6
Nat <natalie.tay@discourse.org>
2025-03-05 17:21:06 +0800

    Reference translations so posts load normally

--- lib/discourse_translator/topic_view_serializer_extension.rb
+++ lib/discourse_translator/topic_view_serializer_extension.rb
@@ -10,12 +10,12 @@ module DiscourseTranslator
         # we will only load them if the user is using a different language that is supported by the site.
         if SiteSetting.experimental_inline_translation && !LocaleMatcher.user_locale_is_default? &&
              LocaleMatcher.user_locale_in_target_languages?
+          locale = InlineTranslation.effective_locale.to_s.gsub("_", "-")
           posts_query =
-            posts_query.includes(:translations).where(
-              translations: {
-                locale: InlineTranslation.effective_locale.to_s.gsub("_", "-"),
-              },
-            )
+            posts_query
+              .includes(:translations)
+              .references(:translations)
+              .where(translations: { locale: [nil, locale] })
         end
         object.instance_variable_set(:@posts, posts_query)
       end
