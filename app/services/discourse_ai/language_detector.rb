7b59baf5f384501e034309ff5404bff480eb35f7
Nat <natalie.tay@discourse.org>
2025-03-07 15:41:30 +0800

    Update detect prompt as it was returning Russian for RUby

--- app/services/discourse_ai/language_detector.rb
+++ app/services/discourse_ai/language_detector.rb
@@ -3,11 +3,12 @@
 module DiscourseAi
   class LanguageDetector
     PROMPT_TEXT = <<~TEXT
-      You are a language expert. Determine the locale for a set of text.
-      - the locale is a language identifier, such as "en" for English, "de" for German, etc
-      - it may include a region identifier, such as "en-GB" for British English, or "zh-CN" for Simplified Chinese
-      - only return the locale
+      You are a language expert and will determine the locale for user-written content.
+      - the locale is a language identifier, such as "en" for English, "de" for German, or "zh-CN" for Simplified Chinese, etc.
+      - use the vocabulary and grammar of content to determine the locale
+      - do not use links or code to determine the locale
       - do not write explanations
+      - only return the locale
     TEXT
 
     def initialize(text)
