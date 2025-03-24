cbfc71d6b3aa54b91a36a3b7045c19a2242062d3
Rafael dos Santos Silva <xfalcox@gmail.com>
2025-03-24 17:07:12 -0300

    DEV: Disable automatic translation via reactive events when automatic rate is zero (#258)
    Also makes reactive translation respect limiting translation to public content setting
    
    Co-authored-by: Keegan George <kgeorge13@gmail.com>

--- lib/discourse_translator/automatic_translations.rb
+++ lib/discourse_translator/automatic_translations.rb
@@ -4,21 +4,38 @@ module DiscourseTranslator
   class AutomaticTranslations
     def inject(plugin)
       plugin.on(:post_process_cooked) do |_, post|
-        if SiteSetting.automatic_translation_target_languages.present? && post.user_id > 0
+        if translatable?(post)
           Jobs.enqueue(:translate_translatable, type: "Post", translatable_id: post.id)
         end
       end
 
       plugin.on(:topic_created) do |topic|
-        if SiteSetting.automatic_translation_target_languages.present? && topic.user_id > 0
+        if translatable?(topic)
           Jobs.enqueue(:translate_translatable, type: "Topic", translatable_id: topic.id)
         end
       end
 
       plugin.on(:topic_edited) do |topic|
-        if SiteSetting.automatic_translation_target_languages.present? && topic.user_id > 0
+        if translatable?(topic)
           Jobs.enqueue(:translate_translatable, type: "Topic", translatable_id: topic.id)
         end
+      end
+    end
+
+    def translatable?(content)
+      return false if SiteSetting.automatic_translation_target_languages.blank?
+      return false if content.user_id <= 0
+      return false if SiteSetting.automatic_translation_backfill_rate <= 0
+      return true unless SiteSetting.automatic_translation_backfill_limit_to_public_content
+
+      public_categories = Category.where(read_restricted: false).pluck(:id)
+
+      if content.is_a(Post)
+        public_categories.include?(content.topic.category_id)
+      elsif content.is_a(Topic)
+        public_categories.include?(content.category_id)
+      else
+        false
       end
     end
   end
