5585b0d4ee9c92218db9aeaea6e109e79570fa84
Nat <natalie.tay@discourse.org>
2025-05-21 17:55:54 +0800

    DEV: Also translate excerpts

--- spec/services/topic_translator_spec.rb
+++ spec/services/topic_translator_spec.rb
@@ -2,15 +2,23 @@
 
 describe DiscourseTranslator::TopicTranslator do
   describe ".translate" do
-    fab!(:topic) { Fabricate(:topic, title: "this is a cat topic :)") }
+    fab!(:topic) do
+      Fabricate(
+        :topic,
+        title: "this is a cat topic :)",
+        excerpt: "cats are great. how many do you have?",
+      )
+    end
     let(:translator) { mock }
     let(:translated_title) { "これは猫の話題です :)" }
+    let(:translated_excerpt) { "猫は素晴らしいですね。何匹飼っていますか？" }
     let(:fancy_title) { "これは猫の話題です :slight_smile:" }
     let(:target_locale) { "ja" }
 
     before do
       DiscourseTranslator::Provider::TranslatorProvider.stubs(:get).returns(translator)
       translator.stubs(:translate_topic!).with(topic, :ja).returns(translated_title)
+      translator.stubs(:translate_text!).with(topic.excerpt, :ja).returns(translated_excerpt)
     end
 
     it "returns nil if topic is blank" do
@@ -30,12 +38,14 @@ describe DiscourseTranslator::TopicTranslator do
 
     it "translates with topic and locale" do
       translator.expects(:translate_topic!).with(topic, :ja).returns(translated_title)
+      translator.expects(:translate_text!).with(topic.excerpt, :ja).returns(translated_title)
 
       described_class.translate(topic, "ja")
     end
 
     it "normalizes dashes to underscores and symbol type for locale" do
       translator.expects(:translate_topic!).with(topic, :zh_CN).returns("这是一个猫主题 :)")
+      translator.expects(:translate_text!).with(topic.excerpt, :zh_CN).returns("这是一个猫主题 :)")
 
       described_class.translate(topic, "zh-CN")
     end
@@ -48,6 +58,7 @@ describe DiscourseTranslator::TopicTranslator do
           topic_id: topic.id,
           locale: target_locale,
           title: translated_title,
+          excerpt: translated_excerpt,
           fancy_title: fancy_title,
           localizer_user_id: Discourse.system_user.id,
         )
@@ -61,6 +72,7 @@ describe DiscourseTranslator::TopicTranslator do
           topic:,
           locale: "ja",
           title: "old title",
+          excerpt: "old excerpt",
           fancy_title: "old_fancy_title",
         )
       expect {
@@ -68,10 +80,12 @@ describe DiscourseTranslator::TopicTranslator do
           id: localization.id,
           title: translated_title,
           fancy_title: fancy_title,
+          excerpt: translated_excerpt,
         )
         expect(localization.reload).to have_attributes(
           title: translated_title,
           fancy_title: fancy_title,
+          excerpt: translated_excerpt,
         )
       }.to_not change { TopicLocalization.count }
     end
